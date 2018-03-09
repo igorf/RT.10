c8636ed488df765853691b420e4189347c8b4fe8
igor <ifedorenko@gmail.com>
2018-03-09 23:04:10 +0400

    indicator library added

---
+++ interface.module/src/RunCounter.cpp
@@ -0,0 +1,1 @@
+#include "RunCounter.h"
---
+++ interface.module/src/RunCounter.h
@@ -0,0 +1,18 @@
+#ifndef INTERFACE_MODULE_RUNCOUNTER_H
+#define INTERFACE_MODULE_RUNCOUNTER_H
+
+
+class RunCounter {
+public:
+    //TODO: Выводить счетчик при изменении в индикатор
+    //void init();
+    //void inc();
+    //void reset();
+
+private:
+    int count = 0;
+    //void display();
+};
+
+
+#endif //INTERFACE_MODULE_RUNCOUNTER_H
