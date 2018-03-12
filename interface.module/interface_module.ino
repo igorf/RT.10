00a86ffb8d010efbb0265a083695f02ee56b3b92
igor <ifedorenko@gmail.com>
2018-03-12 11:15:26 +0400

    timer added

--- interface.module/interface_module.ino
+++ interface.module/interface_module.ino
@@ -2,6 +2,7 @@
 #include "src/CommandSender.h"
 #include "src/ModeSelector.h"
 #include "src/RunStarter.h"
+#include "TimerOne.h"
 
 ModeSelector modeSelector;
 RunStarter runStarter;
@@ -11,17 +12,33 @@ const uint8_t SENDER_PIN = 1;
 const uint8_t HORNS_PIN = 5;
 const uint8_t START_PIN = 3;
 
+int nextCommand = -1;
+
 void setup() {
     modeSelector.init(ModeSelector::MODE_SLOW);
     runStarter.init(HORNS_PIN, START_PIN);
     commandSender->init(SENDER_PIN);
+
+    Timer1.initialize(4000000);
+    Timer1.stop();
+    Timer1.restart();
+    Timer1.attachInterrupt(timerIsr);
 }
 
 void loop() {
     modeSelector.select();
     if (runStarter.check()) {
-        int mode = modeSelector.getMode();
+        nextCommand = modeSelector.getMode();
         Serial.print("Start!\n\n");
+        Timer1.start();
     }
     delay(10);
+}
+
+void timerIsr() {
+    Timer1.stop();
+    Timer1.restart();
+    Serial.print("!!!\n");
+    commandSender->send(nextCommand);
+    runStarter.arm();
 }
