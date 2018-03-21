f1cd733b77ce1492777324dc5720999d431bc21b
igor <ifedorenko@gmail.com>
2018-03-22 00:06:06 +0400

    Несколько переаботал отправку

--- interface.module/src/CommandSender.cpp
+++ interface.module/src/CommandSender.cpp
@@ -7,12 +7,13 @@ void CommandSender::init(uint8_t senderPort) {
     Serial.begin(9600);
 
     vw_set_tx_pin(port);
-    vw_setup(4000);
+    vw_set_ptt_inverted(true);
+    vw_setup(2000);
 }
 
 void CommandSender::send(int command) {
     int data[1] = {command};
     vw_send((uint8_t *)data, sizeof(data));
-    //vw_wait_tx();
+    vw_wait_tx();
     Serial.println(command);
 }
