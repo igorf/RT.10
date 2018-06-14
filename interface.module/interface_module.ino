f787566b7d9b64a79a2895921d964b3898f4d212
Игорь Федоренко <pointer@nm.ru>
2018-06-14 23:47:25 +0400

    - Вынес константы из проекта в отдельный файл.

--- interface.module/interface_module.ino
+++ interface.module/interface_module.ino
@@ -4,6 +4,7 @@
 #include "src/RunStarter.h"
 #include "src/RunCounter.h"
 #include "src/RunControl.h"
+#include "src/GlobalConstants.h"
 #include <MsTimer2.h>
 
 ModeSelector    *modeSelector   = new ModeSelector();
@@ -12,30 +13,15 @@ CommandSender   *commandSender  = new CommandSender();
 RunCounter      *runCounter     = new RunCounter();
 RunControl      *runControl     = new RunControl();
 
-//TODO: Выексти в константы
-const uint8_t STOP_PIN          = 2;
-const uint8_t START_PIN         = 3;
-const uint8_t RESET_PIN         = 4;
-const uint8_t HORNS_PIN         = 5;
-const uint8_t CANCEL_PIN        = 6;
-const uint8_t CNT_SCLK_PIN      = 8;
-const uint8_t CNT_RCLK_PIN      = 9;
-const uint8_t CNT_DIO_PIN       = 10;
-const uint8_t SENDER_PIN        = 11;
-const uint8_t START_LIGHT_PIN   = A4;
-const uint8_t COUNT_RESET_PIN   = A5;
-
-const unsigned long RUN_DELAY = 4000;
-
 int nextCommand = -1;
 
 void setup() {
-    runCounter->init(CNT_SCLK_PIN, CNT_RCLK_PIN, CNT_DIO_PIN, COUNT_RESET_PIN);
+    runCounter->init(GlobalConstants::CNT_SCLK_PIN, GlobalConstants::CNT_RCLK_PIN, GlobalConstants::CNT_DIO_PIN, GlobalConstants::COUNT_RESET_PIN);
     modeSelector->init(ModeSelector::MODE_SLOW);
-    runStarter->init(HORNS_PIN, START_PIN, CANCEL_PIN, START_LIGHT_PIN);
-    runControl->init(STOP_PIN, RESET_PIN);
-    commandSender->init(SENDER_PIN);
-    MsTimer2::set(RUN_DELAY, startTarget);
+    runStarter->init(GlobalConstants::HORNS_PIN, GlobalConstants::START_PIN, GlobalConstants::CANCEL_PIN, GlobalConstants::START_LIGHT_PIN);
+    runControl->init(GlobalConstants::STOP_PIN, GlobalConstants::RESET_PIN);
+    commandSender->init(GlobalConstants::SENDER_PIN);
+    MsTimer2::set(GlobalConstants::RUN_DELAY, startTarget);
 }
 
 void loop() {
--- interface.module/src/GlobalConstants.h
+++ interface.module/src/GlobalConstants.h
@@ -10,6 +10,20 @@ class GlobalConstants {
 public:
     static const uint8_t LIGHT_MODE_ON = LOW;
     static const uint8_t LIGHT_MODE_OFF = HIGH;
+
+    static const uint8_t STOP_PIN          = 2;
+    static const uint8_t START_PIN         = 3;
+    static const uint8_t RESET_PIN         = 4;
+    static const uint8_t HORNS_PIN         = 5;
+    static const uint8_t CANCEL_PIN        = 6;
+    static const uint8_t CNT_SCLK_PIN      = 8;
+    static const uint8_t CNT_RCLK_PIN      = 9;
+    static const uint8_t CNT_DIO_PIN       = 10;
+    static const uint8_t SENDER_PIN        = 11;
+    static const uint8_t START_LIGHT_PIN   = A4;
+    static const uint8_t COUNT_RESET_PIN   = A5;
+
+    static const unsigned long RUN_DELAY = 4000;
 };
 
 
